eea838a94e4b37d42ad23e86ebfaef9136ea5acb
huangyCode <254700023@qq.com>
2020-03-03 00:11:25 +0800

    init

--- config/conf.js
+++ config/conf.js
@@ -1,4 +1,4 @@
 module.exports = {
-  development: 'http://47.114.129.233:8090/alc-backend',
-  production: 'http://47.114.129.233:8080/alc-backend',
+  production: 'http://47.114.129.233:8090/alc-backend',
+  '': 'http://47.114.129.233:8080/alc-backend',
 }[process.env.NODE_ENV || 'development'];
