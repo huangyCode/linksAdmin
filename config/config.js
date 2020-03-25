4d8106f9b6d317f8f410767f37976d771bedd0ed
huangyCode <254700023@qq.com>
2020-03-25 10:48:49 +0800

    达达配送

--- config/config.js
+++ config/config.js
@@ -233,7 +233,7 @@ export default {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION:
       ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION || '', // preview.pro.ant.design only do not use in your production ; preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
     'process.env': {
-      NODE_ENV: process.env.NODE_ENV,    // 这里是重点吧，获取配置
+      API_ENV: process.env.API_ENV,    // 这里是重点吧，获取配置
     },
   },
   ignoreMomentLocale: true,
